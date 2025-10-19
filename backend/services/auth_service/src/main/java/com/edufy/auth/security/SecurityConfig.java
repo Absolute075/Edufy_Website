dc58a59eaba4e97d8e925efcbf65fb2a71f09776
Absolute075 <absolute07589@gmail.com>
2025-10-20 01:36:52 +0500

    Register

--- backend/services/auth_service/src/main/java/com/edufy/auth/security/SecurityConfig.java
+++ backend/services/auth_service/src/main/java/com/edufy/auth/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.edufy.auth.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,10 +25,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login").permitAll() // публичные маршруты
+                        .requestMatchers("/", "/index.html", "/register.html", "/auth/**").permitAll()
                         .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults()); // оставляем для внутренних нужд
+                );
+
 
         return http.build();
     }
