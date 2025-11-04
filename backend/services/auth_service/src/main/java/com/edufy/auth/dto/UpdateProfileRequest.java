d4cf170e0907e0ac657f2fd71d8738d763986854
Absolute075 <absolute07589@gmail.com>
2025-11-04 13:35:51 +0500

    tuda syuda

---
+++ backend/services/auth_service/src/main/java/com/edufy/auth/dto/UpdateProfileRequest.java
@@ -0,0 +1,13 @@
+package com.edufy.auth.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UpdateProfileRequest {
+    private String phone;
+    private String location;
+    // ISO date string: YYYY-MM-DD
+    private String birthDate;
+}
+
