accd26ef79a5872b9de4cb2ed679cb9fccd7f189
Absolute075 <absolute07589@gmail.com>
2025-12-08 13:13:44 +0500

    tuda syuda

--- backend/db/init/02-create-edufy-users.sql
+++ backend/db/init/02-create-edufy-users.sql
@@ -1,11 +1,2 @@
--- Create additional database for user_service if it doesn't exist
-DO
-$$
-BEGIN
-   IF NOT EXISTS (
-      SELECT 1 FROM pg_database WHERE datname = 'edufy_users'
-   ) THEN
-      EXECUTE 'CREATE DATABASE edufy_users';
-   END IF;
-END
-$$;
+-- Deprecated: previously created separate 'edufy_users' database.
+-- All services now use a single Postgres database named 'edufy'.
