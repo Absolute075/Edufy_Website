a2b6f14315d2fb6337ae929db7f2092d30246ae5
Absolute075 <absolute07589@gmail.com>
2025-12-29 18:39:40 +0500

    tuda syuda

--- frontend/react/next.config.mjs
+++ frontend/react/next.config.mjs
@@ -20,24 +20,6 @@ const nextConfig = {
           },
         ],
       },
-      {
-        source: '/_next/static/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/_next/image/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
     ];
   },
   async rewrites() {
