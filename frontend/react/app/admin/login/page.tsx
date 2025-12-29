ebb79a31e3ced6e925ea6a1fe895283cc2195b23
Absolute075 <absolute07589@gmail.com>
2025-12-29 21:18:28 +0500

    tuda syuda

--- frontend/react/app/admin/login/page.tsx
+++ frontend/react/app/admin/login/page.tsx
@@ -21,6 +21,7 @@ export default function AdminLoginPage() {
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify({ username, password }),
       });
 
