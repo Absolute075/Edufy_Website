0cfdb0b98e7157f1c89ede5514f99ec3fdb2fd96
Absolute075 <absolute07589@gmail.com>
2026-01-02 23:07:01 +0500

    tuda syuda

--- frontend/react/lib/resourcesRegistry.ts
+++ frontend/react/lib/resourcesRegistry.ts
@@ -353,6 +353,15 @@ export const resourcesRegistry: Record<ResourceCategory, Record<string, Resource
       minutes: 20,
       questions: 14,
     },
+    "966855": {
+      requiredPlan: "premium",
+      title: "Olive Oil Production",
+      examType: "real",
+      part: 3,
+      difficulty: "hard",
+      minutes: 20,
+      questions: 13,
+    },
   },
   listening: {
     "846376": {
